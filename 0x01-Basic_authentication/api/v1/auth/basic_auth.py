ff2d4b94e80c57e2ba9fd21cd1dbc5df76d1fe63
T-sosthenese <sosthenetimi94@gmail.com>
2024-01-16 14:36:45 +0300

    Finished basic auth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,7 +3,11 @@
 Implementing Basic Authentication
 """
 from api.v1.auth.auth import Auth
-from typing import Tuple
+from typing import Tuple, TypeVar
+import uuid
+import hashlib
+from models.user import User
+from models.base import DATA
 import base64
 import re
 
@@ -44,3 +48,24 @@ class BasicAuth(Auth):
                 password = re.match('.+:(.+)', decoded_base)[1]
                 return (email, password)
         return (None, None)
+
+    def user_object_from_credentials(self, user_email: str,
+                                     user_pwd: str) -> TypeVar('User'):
+        """Returns users with their current credentials"""
+        if user_email is None or type(user_email) != str or \
+                user_pwd is None or type(user_pwd) != str:
+            return None
+        try:
+            for user in User.search({'email': user_email}):
+                if user.is_valid_passsword(user_pwd):
+                    return user
+        except Exception:
+            return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Overloads Auth and retrieves the User instance"""
+        auth_header = self.authorization_header(request)
+        base64_header = self.extract_base64_authorization_header(auth_header)
+        decoded_header = self.decode_base64_authorization_header(base64_header)
+        email, pwrd = self.extract_user_credentials(decoded_header)
+        return self.user_object_from_credentials(email, pwrd)
