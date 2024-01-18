afb747e1d1f85c564cd218771a9beeaaaa2520ea
T-sosthenese <sosthenetimi94@gmail.com>
2024-01-18 15:01:03 +0300

    session authentication

---
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+Authentication module
+"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """Implemetation of a class for autheticating users"""
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Returns True if path is not in excluded_paths"""
+        if path is not None and excluded_paths is not None:
+            if path[-1] != "/":
+                path = path + "/"
+            if path in excluded_paths:
+                return False
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """Validates all API requests for secure API"""
+        if request is not None:
+            dict_key = request.headers.get('Authorization')
+            if dict_key is not None:
+                return dict_key
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Returns the current user"""
+        return None
