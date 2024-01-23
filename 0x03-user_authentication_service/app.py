2499451f80ea65fe8ae540117da34e6c6c139f81
T-sosthenese <sosthenetimi94@gmail.com>
2024-01-23 16:30:52 +0300

    Get reset password token

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -65,7 +65,7 @@ def profile() -> str:
     return jsonify({"email": user.email})
 
 
-@app.route("/reset_password", methods=["POST", strict_slashes=False])
+@app.route("/reset_password", methods=["POST"], strict_slashes=False)
 def get_reset_password_token() -> str:
     """
     User's password reset payload
